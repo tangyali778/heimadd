eeab140058225f83ed9dcda60d35f596f1fff226
tangyali778 <63383047+tangyali778@users.noreply.github.com>
2020-06-04 18:35:06 +0800

    设置导航守卫以及请求响应拦截器

--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,43 @@ import VueRouter from 'vue-router'
 //导入组件
 import Login from '@/views/login'
 import Layout from '@/views/layout'
+//按需导入token
+import {
+    getToken
+} from '@/utils/token.js'
 //注册路由
 Vue.use(VueRouter)
 //实例化
 const router = new VueRouter({
-    routes: [
-        {path:'/login',component:Login},
-        {path:'/layout',component:Layout},
-        {path:'/',redirect:'/login'}
+    routes: [{
+            path: '/login',
+            component: Login
+        },
+        {
+            path: '/layout',
+            component: Layout
+        },
+        {
+            path: '/',
+            redirect: '/login'
+        }
     ]
+})
+//导航守卫
+router.beforeEach((to, from, next) => {
+    // console.log(to);
+
+    if (to.fullPath == '/login') {
+        next()
+    } else {
+        const token = getToken()
+        if (token) {
+            next() //去到自己想去的地方
+        } else {
+            next('/login')
+        }
+    }
+
+
 })
 export default router
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import axios from 'axios'
+//按需导入token
+import {getToken,removeToken} from 'utils/token.js'
+import router from '../router';
 
 // axios的基本路径
 axios.defaults.baseURL = process.env.VUE_APP_BASEURL
@@ -7,3 +10,32 @@ axios.defaults.baseURL = process.env.VUE_APP_BASEURL
 axios.defaults.withCredentials = true;
 // axios全局配置
 Vue.prototype.$axios = axios
+
+// 请求拦截器
+axios.interceptors.request.use(function (config) {
+    // 发送请求就把token带过去
+    const token = getToken()
+    if (token) {
+        config.header.token = token;
+    }
+
+    return config;
+   
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
+//响应拦截器
+axios.interceptors.response.use(function (response) {
+    // token过期的话(返回的code就是206)就把token删掉并且回到登录页
+     if (response.data.code == 206) {
+         removeToken();
+         router.push('/login')
+         return
+     }
+    return response;
+  }, function (error) {
+    // Do something with response error
+    return Promise.reject(error);
+  });
